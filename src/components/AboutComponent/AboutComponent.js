57aa1395fd3f902c9bf12cae145f5b8bdf79d0d1
dallynowens2 <dallynowens2@gmail.com>
2021-11-07 20:51:57 -0700

    more spelling changes

--- src/components/AboutComponent/AboutComponent.js
+++ src/components/AboutComponent/AboutComponent.js
@@ -10,10 +10,10 @@ const AboutComponent = () => {
             <div className='col-10'>
                 <h1 className="justify-content-center text-light display-5 p-2">About Me</h1>
                 <div className='text-light lead p-2'>
-                    I am a software engineering student at snow college currently. I have experience in using many different languages such
+                    I am a software engineering student at Snow College currently. I have experience in using many different languages such
                     as C#, JavaScript, Kotlin, Python, Java, SQL and many others. I have worked in different enviorments such as using visual studio, VS code and android studio.
                     I have used different cloud services and containers such as Azure and Docker. Some of my goals are to always be learning and improving my abilities, to be part of a great 
-                    team that is putting out great software and to continual get better at writing code that is clean, easy to understand, and testable.
+                    team that is putting out great software and to continually get better at writing code that is clean, easy to understand, and testable.
                 </div>
             </div>
             
